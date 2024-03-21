b33feddd1645911dec73816cc4260f253e2fbc58
Aleksei Seletskiy <lesha.seletskiy@gmail.com>
2024-03-21 15:22:37 -0400

    remove harcoded paths and fix link time errors

--- install_folly.sh
+++ install_folly.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+mkdir -p test_install
 mv install /tmp
 cd folly
 git checkout main
 git pull
 mkdir -p install
 sudo ./build/fbcode_builder/getdeps.py install-system-deps --recursive
-python3 ./build/fbcode_builder/getdeps.py --allow-system-packages build
+python3 ./build/fbcode_builder/getdeps.py --allow-system-packages build --install-dir=../test_install
 # python3 ./build/fbcode_builder/getdeps.py --allow-system-packages test
 FOLLY_PATH=$(python3 ./build/fbcode_builder/getdeps.py show-build-dir)
 cp -r $FOLLY_PATH/../../installed/ ../install
--- scripts/throughput/install.sh
+++ scripts/throughput/install.sh
@@ -7,4 +7,4 @@ git checkout other_benchmarks
 git pull
 mkdir -p build
 cd build
-cmake .. -DCDRC_BENCHMARK=On -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=make -DCMAKE_PREFIX_PATH=../../install/folly/lib/cmake/folly .. && make && echo "Built throughputs benchmarks, see concurrent_deferred_rc/build/benchmarks"
+cmake .. -DCDRC_BENCHMARK=On -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="../../install/folly/lib/cmake/folly;../../install/fmt/lib/cmake/fmt" .. && make && echo "Built throughputs benchmarks, see concurrent_deferred_rc/build/benchmarks"
