70aacaddb2dd697fe510b361d5baa94287164f62
Aleksei Seletskiy <lesha.seletskiy@gmail.com>
2024-03-20 20:03:53 -0400

    folly attempt two (broken)

--- install_folly.sh
+++ install_folly.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+mv install /tmp
 cd folly
+git checkout main
 git pull
+mkdir -p install
 sudo ./build/fbcode_builder/getdeps.py install-system-deps --recursive
 python3 ./build/fbcode_builder/getdeps.py --allow-system-packages build
 # python3 ./build/fbcode_builder/getdeps.py --allow-system-packages test
 FOLLY_PATH=$(python3 ./build/fbcode_builder/getdeps.py show-build-dir)
-cp $FOLLY_PATH/libfolly.a ..
+cp -r $FOLLY_PATH/../../installed/ ../install
+
+cd ..
+mv install/fmt*/ install/fmt
+mv install/googletest* install/googletest
